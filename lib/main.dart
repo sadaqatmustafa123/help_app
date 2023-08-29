3107e521abebf6ce4754b2953c99e05410c6db3f
sadaqatmustafa123 <sadaqatmustafa12345@gmail.com>
2023-08-30 02:31:31 +0500

    added new code to the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -7,7 +8,7 @@ void main() {
 }
 
 class HelpSection extends StatelessWidget {
-  const HelpSection({super.key});
+  const HelpSection({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -22,25 +23,117 @@ class HelpSection extends StatelessWidget {
             style: TextStyle(color: Colors.yellow),
           ),
         ),
-        body: Column(children: [
-          SizedBox(
-            height: 45,
-          ),
-          Center(
-            child: Image.asset(
-              'images/contactus2.png',
-              height: 250,
+        body: Column(
+          children: [
+            SizedBox(
+              height: 45,
             ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Text(
-            "Have any query or issue? \nFeel free to contact us",
-            textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 20, color: Colors.grey[800]),
-          ),
-        ]),
+            Center(
+              child: Image.asset(
+                'images/contactus2.png',
+                height: 250,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Have any query or issue?\nFeel free to contact us",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 20, color: Colors.grey[800]),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 100,
+                    width: 150,
+                    color: Colors.grey,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.alternate_email,
+                          color: Colors.orange,
+                          size: 50,
+                        ),
+                        Text("Write to us."),
+                        Text("help@gmail.com")
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 100,
+                    width: 150,
+                    color: Colors.grey,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.phone_android,
+                          color: Colors.orange,
+                          size: 50,
+                        ),
+                        Text("Write to us."),
+                        Text("help@gmail.com")
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 100,
+                    width: 150,
+                    color: Colors.grey,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.contact_mail,
+                          color: Colors.orange,
+                          size: 50,
+                        ),
+                        Text("Write to us."),
+                        Text("help@gmail.com")
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 100,
+                    width: 150,
+                    color: Colors.grey,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.location_city,
+                          color: Colors.orange,
+                          size: 50,
+                        ),
+                        Text("Write to us."),
+                        Text("help@gmail.com")
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
